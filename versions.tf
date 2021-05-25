51780777aba40a04c9c8440595b69773bbb17de9
smulampa <smulampa@in.ibm.com>
2021-05-25 18:30:58 +0530

    changes

---
+++ versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    ibm = {
+      # TF-UPGRADE-TODO
+      #
+      # No source detected for this provider. You must add a source address
+      # in the following format:
+      #
+      # source = "IBM-Cloud/ibm"
+      #
+      # For more information, see the provider source documentation:
+      #
+      # https://www.terraform.io/docs/configuration/providers.html#provider-source
+    }
+  }
+  required_version = ">= 0.13"
+}
